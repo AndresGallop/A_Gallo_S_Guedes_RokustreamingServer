9616a660e90150ea8fdd060a550449936e8a27a1
AndresGallop <andresgallod.i@gmail.com>
2021-04-15 02:27:14 -0400

    SignUp user Query added

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
+//const connection = require('../config/sqlConfig');
 // express router handles incoming requests and deirects them where they need to go
 // like a traffic cop
 const router = express.Router();
+//import the sql connection
+const connect = require("../config/sqlConfig");
 
 //think of route like PHP functions
 
@@ -14,26 +17,76 @@ router.get("/", (req, res) => {
 // this is /api/users route handler
 router.get("/users", (req, res) => {
     //run a SQL query here
+
+    //******
+    
+    connect.getConnection(function(err, connection) {
+      if (err) throw err; // not connected!
+     
+      // Use the connection
+      connection.query('SELECT * FROM tbl_user', function (error, results) {
+        // When done with the connection, release it.
+        connection.release();
+     
+        // Handle error after the release.
+        if (error) throw error;
+
+        res.json(results);
+      });
+    });
+
+    //******
+
     // res.json(query result here)
     // echo a result --> just like PHP
-    res.json({message: "allusers route"});
+    //res.json({message: "all users route"});
+})
+
+router.get("/users/:id", (req, res) => {
+  //run a SQL query here to get all the movies from my DB
+  connect.query(`SELECT * FROM tbl_user WHERE user_id=${req.params.id}`, function (error, results, fields) {
+
+      if (error) throw error;
+
+      console.log("results:", results, "fields:", fields);
+      res.json(results);
+    });
+  
 })
 
 router.get("/movies", (req, res) => {
     //run a SQL query here to get all the movies from my DB
-    // res.json(query result here)
-    // echo a result --> just like PHP
-    res.json({message: "all movies route"});
+    connect.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT * FROM tbl_movies', function (error, results) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+
+          res.json(results);
+        });
+      });
 })
+
+
 
 //dynamic route handler that can accept a parameter
 //this is equivalent to $_GET["id"] (req.params.id)
 //you're passing the id via the route: /api/movies/1 , /api/movies/20, etc
 router.get("/movies/:id", (req, res) => {
     //run a SQL query here to get all the movies from my DB
-    // res.json(query result here)
-    // echo a result --> just like PHP
-    res.json({message: "get one movie route", movie: req.params.id});
+    connect.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function (error, results, fields) {
+
+        if (error) throw error;
+
+        console.log("results:", results, "fields:", fields);
+        res.json(results);
+      });
+    
 })
 
 module.exports = router;
